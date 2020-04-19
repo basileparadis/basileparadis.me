f8d0b10ff29b3543524541ee1682bc6591259895
pthomas <pthomas@aibee.com>
2020-04-19 17:05:33 -0400

    added fetart website

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,9 +6,7 @@
       <SinglePageElement>
         <div slot="content">
           <div class="title">Hi !</div>
-          <p>
-            I am a full-stack (backend leaning) software engineer, passionate about web technologies and AI.
-          </p>
+          <p>I am a full-stack (backend leaning) software engineer, passionate about web technologies and AI.</p>
           <br />
           <p>
             I graduated with a master's degree from Tsinghua university in Beijing. Then, I was one of the early employees at Aibee, an AI startup where I
@@ -16,16 +14,16 @@
             designed, implemented and deployed multiple web services for demonstration to our clients, under tight deadlines.
           </p>
           <br />
-          <p>
-            In my free time, I enjoy playing guitar, working on personnal software projects and cooking.
-          </p>
-          <p>
-            I am also a language-learning enthusiast. I speak Chinese, English, French and Spanish, currently learning Russian.
-          </p>
+          <p>In my free time, I enjoy playing guitar, working on personnal software projects and cooking.</p>
+          <p>I am also a language-learning enthusiast. I speak Chinese, English, French and Spanish, currently learning Russian.</p>
           <br />
           <div class="level">
-            <a class="download-link level-item" href="https://github.com/Cephalopode"> <fa-icon :icon="['fab', 'github']" />Github </a>
-            <a class="download-link level-item" href="https://www.linkedin.com/in/thomas-paradis-8ab6766a"> <fa-icon :icon="['fab', 'linkedin']" />LinkedIn </a>
+            <a class="download-link level-item" href="https://github.com/Cephalopode">
+              <fa-icon :icon="['fab', 'github']" />Github
+            </a>
+            <a class="download-link level-item" href="https://www.linkedin.com/in/thomas-paradis-8ab6766a">
+              <fa-icon :icon="['fab', 'linkedin']" />LinkedIn
+            </a>
           </div>
         </div>
         <img slot="media" class="medaillon" :src="require('@/assets/img/photos/profil.jpg')" />
@@ -56,10 +54,33 @@
             <a class="download-link level-item" href="https://chrome.google.com/webstore/detail/wiki-popup/needbkjfeeapnlgefnpoebhojcogflej">
               <fa-icon :icon="['fab', 'chrome']" />Add to Chrome
             </a>
-            <a class="download-link level-item" href="https://github.com/Cephalopode/Wiki_popup"> <fa-icon :icon="['fab', 'github']" />Github </a>
+            <a class="download-link level-item" href="https://github.com/Cephalopode/Wiki_popup">
+              <fa-icon :icon="['fab', 'github']" />Github
+            </a>
           </div>
         </div>
         <img slot="media" class="image" :src="require('@/assets/img/illustrations/wiki-popup.png')" />
+      </SinglePageElement>
+      <SinglePageElement reversed>
+        <div slot="content">
+          <div class="title">Fêt'Art: a simple eCommerce website</div>
+          <p>A small-scale eCommerce website, which allows customer to easily place an order through email. The product inventory and photos can be edited in real-time by the seller, using an online Google Spreadsheet.</p>
+          <br />
+          <p>
+            Designed using the Vue.js design framework
+            <a href="https://www.storefrontui.io/">StorefrontUi</a>. Backend uses Express.js to query the Google Spreadsheet API.
+          </p>
+          <br />
+          <div class="level">
+            <a class="download-link level-item" href="http://fetart.thomasparadis.me">
+              <fa-icon :icon="['fas', 'play-circle']" />Website
+            </a>
+            <a class="download-link level-item" href="https://github.com/Cephalopode/fetart">
+              <fa-icon :icon="['fab', 'github']" />Github
+            </a>
+          </div>
+        </div>
+        <img slot="media" class="image" :src="require('@/assets/img/illustrations/fetart.png')" />
       </SinglePageElement>
       <SinglePageElement reversed>
         <div slot="content">
@@ -76,8 +97,12 @@
           </p>
           <br />
           <div class="level">
-            <a class="download-link level-item" href="/phonetic_dict"> <fa-icon :icon="['fas', 'play-circle']" />Website </a>
-            <a class="download-link level-item" href="https://github.com/Cephalopode/PhoneticDict_Web"> <fa-icon :icon="['fab', 'github']" />Github </a>
+            <a class="download-link level-item" href="/phonetic_dict">
+              <fa-icon :icon="['fas', 'play-circle']" />Website
+            </a>
+            <a class="download-link level-item" href="https://github.com/Cephalopode/PhoneticDict_Web">
+              <fa-icon :icon="['fab', 'github']" />Github
+            </a>
           </div>
         </div>
         <img slot="media" class="image" :src="require('@/assets/img/illustrations/phonetic_dict.png')" />
@@ -88,11 +113,15 @@
           <p>
             A web crawler API that fetches all images on a given domain. Written as an excercice to test Go language concurrency features. It is available for
             demo purposes
-            <a href="thomasparadis.me:8080">Here</a> (API only, no frontend).
+            <a
+              href="thomasparadis.me:8080"
+            >Here</a> (API only, no frontend).
           </p>
           <br />
           <div class="level">
-            <a class="download-link level-item" href="https://github.com/Cephalopode/mims_web_crawler"> <fa-icon :icon="['fab', 'github']" />Github </a>
+            <a class="download-link level-item" href="https://github.com/Cephalopode/mims_web_crawler">
+              <fa-icon :icon="['fab', 'github']" />Github
+            </a>
           </div>
         </div>
         <img slot="media" class="image" style="height:240px; margin:-50px" :src="require('@/assets/img/illustrations/icons/laptop-globe.svg')" />
@@ -107,7 +136,7 @@
         <div slot="content">
           <div class="title">Contact</div>
           <br />
-          <p class="subtitle">Let's get in touch!</p>
+          <p class="subtitle">Let's get in touch! Leave me a message and I'll reply as soon as possible</p>
           <div class="form-module--component--1t7o4">
             <div class="block">
               <label class="label">Email</label>
@@ -117,21 +146,11 @@
               <label class="label">Message</label>
               <textarea class="textarea" name="message" v-model="message" required></textarea>
             </div>
-            <vue-recaptcha
-              class="block"
-              ref="captcha"
-              sitekey="6LeYjOYUAAAAACuo69suXOKrloy9Ght4KaSpAjhY"
-              :loadRecaptchaScript="true"
-              @verify="setVerified"
-            ></vue-recaptcha>
+            <vue-recaptcha class="block" ref="captcha" sitekey="6LeYjOYUAAAAACuo69suXOKrloy9Ght4KaSpAjhY" :loadRecaptchaScript="true" @verify="setVerified"></vue-recaptcha>
 
             <div class="level" style="width:300px">
-              <button class="level-left button is-primary" @click="sendEmail()">
-                Send
-              </button>
-              <p v-if="verifyWarning" class="level-left captcha-warning">
-                Please solve captcha first
-              </p>
+              <button class="level-left button is-primary" @click="sendEmail()">Send</button>
+              <p v-if="verifyWarning" class="level-left captcha-warning">Please solve captcha first</p>
             </div>
           </div>
           <EmailModal />
@@ -149,15 +168,20 @@
     <footer class="footer">
       <div class="content has-text-centered">
         <p>
-          Designed and developped by <a href="#">Thomas Paradis</a>, using <a href="https://cli.vuejs.org/">Vue.js</a>, <a href="https://bulma.io/">Bulma</a>,
-          <a href="https://sass-lang.com/">SASS</a> and <a href="https://expressjs.com/">Node.js+Express</a>. Inspired by
-          <a href="https://jgthms.com/"> Jeremy Thomas.</a>
+          Designed and developped by
+          <a href="#">Thomas Paradis</a>, using
+          <a href="https://cli.vuejs.org/">Vue.js</a>,
+          <a href="https://bulma.io/">Bulma</a>,
+          <a href="https://sass-lang.com/">SASS</a> and
+          <a href="https://expressjs.com/">Node.js+Express</a>. Inspired by
+          <a href="https://jgthms.com/">Jeremy Thomas.</a>
           <br />
 
-          <span class="copyright"
-            >The source code is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed
-            <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.</span
-          >
+          <span class="copyright">
+            The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content is licensed
+            <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+          </span>
         </p>
       </div>
     </footer>
@@ -165,26 +189,26 @@
 </template>
 
 <script>
-import axios from 'axios'
-import VueRecaptcha from 'vue-recaptcha'
-import { singlePageFeatures } from '../utils/single_page'
-import SinglePageElement from './SinglePageElement'
-import Navbar from './Navbar'
-import EmailModal from './EmailModal'
+import axios from "axios"
+import VueRecaptcha from "vue-recaptcha"
+import { singlePageFeatures } from "../utils/single_page"
+import SinglePageElement from "./SinglePageElement"
+import Navbar from "./Navbar"
+import EmailModal from "./EmailModal"
 
 export default {
   components: {
     Navbar,
     SinglePageElement,
     EmailModal,
-    VueRecaptcha,
+    VueRecaptcha
   },
   data: () => ({
-    email: '',
-    message: '',
+    email: "",
+    message: "",
     verified: false,
     verifyWarning: false,
-    captchaToken: '',
+    captchaToken: ""
   }),
   mounted() {
     singlePageFeatures()
@@ -205,37 +229,37 @@ export default {
         return
       }
       axios
-        .post('/api/send-email', {
+        .post("/api/send-email", {
           email: this.email,
           message: this.message,
-          captchaToken: this.captchaToken,
+          captchaToken: this.captchaToken
         })
-        .then((ret) => {
+        .then(ret => {
           console.log(ret)
           const params = {
             success: ret.data.success.toString(),
-            message: ret.data.message,
+            message: ret.data.message
           }
           if (!ret.data.success) {
             this.onSendError(params)
             return
           }
           this.$router.push({ query: params })
-          this.email = ''
-          this.message = ''
+          this.email = ""
+          this.message = ""
         })
-        .catch((err) => {
+        .catch(err => {
           console.log(err)
           const params = {
-            success: 'false',
-            message: err.toString(),
+            success: "false",
+            message: err.toString()
           }
           this.onSendError(params)
         })
       this.$refs.captcha.reset()
       this.verified = false
       this.verifyWarning = false
-    },
-  },
+    }
+  }
 }
 </script>
